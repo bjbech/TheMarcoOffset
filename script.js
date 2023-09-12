b62ef49d63aecef4bd50ef5e392991bd0e9e36da
bbech <45978702+bjbech@users.noreply.github.com>
2023-09-12 14:43:05 -0400

    Initial commit of website

---
+++ script.js
@@ -0,0 +1,30 @@
+function calculateTotal() {
+    // Get the selected iPhone model from the dropdown
+    var selectedModel = document.getElementById("selectedModel").value;
+
+    // Set the starting price based on the selected model
+    var startingPrice;
+    switch (selectedModel) {
+        case "iphone15":
+            startingPrice = 799; // Set the starting price for iPhone 15
+            break;
+        case "iphone15pro":
+            startingPrice = 999; // Set the starting price for iPhone 15 Pro
+            break;
+        default:
+            startingPrice = 0; // Default if none of the options match
+    }
+
+    // Get the input values
+    var num1 = parseFloat(document.getElementById("num1").value) || 0;
+    var num2 = parseFloat(document.getElementById("num2").value) || 0;
+
+    // Calculate the total by adding the input numbers and subtracting the starting price
+    var total = num1 + (num2 * 12) - startingPrice;
+
+    // Ensure the total is displayed as zero if it's less than zero
+    total = total < 0 ? 0 : total;
+
+    // Display the total
+    document.getElementById("total").innerText = total;
+}
